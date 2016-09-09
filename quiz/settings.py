ea7aacb44c6255e435841082bbb5448face0e204
Petronella <petronella.turesson@gmail.com>
2016-09-09 14:01:01 +0200

    Massa settings för Heroku

--- quiz/settings.py
+++ quiz/settings.py
@@ -74,12 +74,14 @@ WSGI_APPLICATION = 'quiz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default='sqlite:////{0}'.format(os.path.join(BASE_DIR,
+'db.sqlite3'))
+    )
 }
+
 
 
 # Password validation
@@ -123,4 +125,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
-
+STATIC_ROOT = 'staticfiles'
