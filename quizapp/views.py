82f8df1f09d4c5523910be5034733ad2e63105d7
Petronella <petronella.turesson@gmail.com>
2016-09-09 14:21:40 +0200

    Fixat

--- quizapp/views.py
+++ quizapp/views.py
@@ -53,8 +53,8 @@ def results(request, quiz_number):
 	num_correct_answers = 0
 	for question_number, answer in saved_answers.items():
 		correct_answer = questions[int(question_number) - 1].correct
-	if correct_answer == answer:
-		num_correct_answers += 1
+		if correct_answer == answer:
+			num_correct_answers += 1
 	context = {
 		"quiz": quiz,
 		"correct": num_correct_answers,
